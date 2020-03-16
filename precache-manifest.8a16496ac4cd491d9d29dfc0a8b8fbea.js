057bf1a48edb12ad6b411b2392bd4aaaeaafd6f7
User-niomic <user@niomic.co.id>
2020-03-16 16:24:22 +0700

    page

---
+++ precache-manifest.8a16496ac4cd491d9d29dfc0a8b8fbea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89ee523ead1e0e40dda908aafceaa771",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcf88a85a703fe3f043b",
+    "url": "/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "9062f5dbc7838d71a76f",
+    "url": "/static/css/main.8b7b808d.chunk.css"
+  },
+  {
+    "revision": "dcf88a85a703fe3f043b",
+    "url": "/static/js/2.26a726f6.chunk.js"
+  },
+  {
+    "revision": "9f007689e4e6f1520dd8392af203676e",
+    "url": "/static/js/2.26a726f6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9062f5dbc7838d71a76f",
+    "url": "/static/js/main.afaa19f3.chunk.js"
+  },
+  {
+    "revision": "147cfbfa7dba3e2bc4d5",
+    "url": "/static/js/runtime-main.837e38cb.js"
+  }
+]);
